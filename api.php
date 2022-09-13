aa24fceb4a3f419b92aa1c3381b975434d93c2ac
MhamadR <mhamadraslan@gmail.com>
2022-09-14 01:11:00 +0300

    Added days until christmas and commented

--- api.php
+++ api.php
@@ -1,13 +1,51 @@
 <?php 
+
+/*Days until christmas*/
+if(date("m") == 12 && date("d") > 25){
+    //Use the timestamp for next year's Christmas Day.
+    $christmasDay = strtotime((date("Y") + 1) . "-12-25");
+} else {
+    $christmasDay = strtotime('December 25');
+}
+
+$time = time();
+$seconds = $christmasDay - $time;
+$days = floor($seconds/86400);
+echo "Days left till next Christmas: " . $days . "<br><br>";
+/***********************/
+
+/*Checking if the input string is a palindrome*/
 if(isset($_GET['string'])) {
     $string = $_GET['string'];
     if(!empty($string)) {
         check_palindrome($string);
     } else {
-        echo "Missing required data";
+        echo "Missing required string<br><br>";
     }
 }
 
+function check_palindrome($string){
+    $string = str_replace(' ', '', $string);
+
+    //remove special characters
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+
+    //change case to lower
+    $string = strtolower($string);
+
+    //reverse the string
+    $reverse = strrev($string);
+
+    if ($string == $reverse) {
+        echo "$string is a palindrome<br><br>";
+    } 
+    else {
+        echo "$string is not a palindrome<br><br>";
+    }
+}
+/***********************/
+
+/*Getting user input and applying a the formula:  a^3 + b*c - a/b */
 if(isset($_POST['varA']) && isset($_POST['varB']) && isset($_POST['varC'])) {
     $a = $_POST['varA'];
     $b = $_POST['varB'];
@@ -15,16 +53,23 @@ if(isset($_POST['varA']) && isset($_POST['varB']) && isset($_POST['varC'])) {
     if(!empty($a) && !empty($b) && !empty($c)){
         applyFormula($a, $b, $c);
     } else {
-        echo "Missing required data";
+        echo "Missing required input numbers<br><br>";
     }
 }
 
+function applyFormula($a, $b, $c) {
+    $result = $a ** $b + $b * $c - $a / $b;
+    echo "a^3 + b*c - a/b = " . $result . "<br><br>";
+}
+/***********************/
+
+/*Checking if password is strong */
 if(isset($_POST['password'])) {
     $password = $_POST['password'];
     if(!empty($password)) {
         checkPassword($password);
     } else {
-        echo "Missing required data";
+        echo "Missing required password<br><br>";
     }
 }
 
@@ -47,38 +92,12 @@ function checkPassword($pass) {
     }
 
     if ($result == ""){
-        echo "Password is strong";
+        echo "Password is strong<br><br>";
     } else {
         echo $result;
     }
 }
-
-function check_palindrome($string){
-    $string = str_replace(' ', '', $string);
-
-    //remove special characters
-    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
-
-    //change case to lower
-    $string = strtolower($string);
-
-    //reverse the string
-    $reverse = strrev($string);
-
-    if ($string == $reverse) {
-        echo "$string is a palindrome";
-    } 
-    else {
-        echo "$string is not a palindrome";
-    }
-}
-
-function applyFormula($a, $b, $c) {
-    // a^3 + b*c - a/b
-    $result = $a ** $b + $b * $c - $a / $b;
-    echo $result;
-}
-
+/***********************/
 ?>
 
 <!DOCTYPE html>
@@ -88,6 +107,11 @@ function applyFormula($a, $b, $c) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP APIs</title>
+    <style>
+        input {display: block;} 
+        form{margin-top: 100px;} 
+        input[type="submit"]{margin-bottom: 20px;}
+    </style>
 </head>
 <body>
     <form action="api.php" method="get">
@@ -96,13 +120,13 @@ function applyFormula($a, $b, $c) {
 
         <input type="submit" value="Submit">
 
-        <label for="varA">Enter a number a: </label>
+        <label for="varA">Enter a number "a": </label>
         <input type="text" name="varA" id="varA">
 
-        <label for="varB">Enter a number b: </label>
+        <label for="varB">Enter a number "b": </label>
         <input type="text" name="varB" id="varB">
 
-        <label for="varC">Enter a number c: </label>
+        <label for="varC">Enter a number "c": </label>
         <input type="text" name="varC" id="varC">
 
         <input type="submit" value="Submit using post" formmethod="post">
@@ -111,5 +135,6 @@ function applyFormula($a, $b, $c) {
         <input type="text" name="password" id="pass">
 
         <input type="submit" value="Submit using post" formmethod="post">
+    </form>
 </body>
 </html>
